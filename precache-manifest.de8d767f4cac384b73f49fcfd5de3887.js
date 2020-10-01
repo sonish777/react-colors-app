1dcdead3d7c88568932b89c1a50fc8c2f852eca9
Sonish Maharjan <42803142+sonish777@users.noreply.github.com>
2020-10-01 23:52:35 +0545

    Updates

---
+++ precache-manifest.de8d767f4cac384b73f49fcfd5de3887.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac0b99795925a1796eddd993d9717c1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2cf3350f66326eb3004f",
+    "url": "/static/css/2.2a4460b4.chunk.css"
+  },
+  {
+    "revision": "7a4d69bc735271b736c0",
+    "url": "/static/css/main.95110e0f.chunk.css"
+  },
+  {
+    "revision": "2cf3350f66326eb3004f",
+    "url": "/static/js/2.a2ddbef1.chunk.js"
+  },
+  {
+    "revision": "fe2f19c5d5965d51615a596cf8d07fe9",
+    "url": "/static/js/2.a2ddbef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a4d69bc735271b736c0",
+    "url": "/static/js/main.44378f7a.chunk.js"
+  },
+  {
+    "revision": "a487204b8a980af72a07",
+    "url": "/static/js/runtime-main.6350e70c.js"
+  }
+]);
